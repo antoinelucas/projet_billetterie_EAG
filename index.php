105fef75c92561665b4b9b797e7bdca48b40b377
Julio-14 <sanchez-jules@orange.fr>
2018-03-20 17:18:15 +0100

    ok google 1 Julio

--- index.php
+++ index.php
@@ -4,5 +4,5 @@
 		session_start();
 	}
 
-include_once '/templates/header.php'
+include_once '/presentation/header.php'
 ?>
---
+++ presentation/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Billetterie_EAG">
+    <meta name="author" content="Jules Hugo Antoine">
+    <title>Billetterie EAG</title>
+    <link href="/css/bootstrap-3.3.7-dist/css/bootstrap-theme.css" rel="stylesheet">
+    <link href="style_eag.css" rel="stylesheet">
+  </head>
+  <nav>
+    <div class="container_nav">
+      <p>Bonjour
+        <?php
+		      include ('../donnees/bd_connexion.php');
+          $reponse = $bdd -> query('SELECT NOM from USERS ');
+
+          while ($donnees = $reponse->fetch())
+          {
+            echo "<h1>bonjour</h1>";
+            echo $donnees['NOM'];
+          }
+        ?>
+      </p>
+    </div>
+    <div class="container_nav">
+      <a class="nav-link" href="register.php">S'inscrire</a>
+    </div>
+    <div class="container_nav">
+      <a class="nav-link" href="register.php">S'inscrire</a>
+    </div>
+
+  </nav>
